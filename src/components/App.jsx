6367926f3dca923ac55c4a388c7e25cf4a31cab7
Jaroslaw Zabiello <hipertracker@gmail.com>
2015-03-07 13:12:26 +0000

    tuning

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react/addons';
 
 class App extends React.Component {
     render() {
-        return <div>Hello World for React 0.13.0-rc2</div>;
+        return <div>Hello React 0.13.0-rc2</div>;
     }
 };
 
--- src/startup.js
+++ src/startup.js
@@ -6,10 +6,11 @@ function* foo() {
     yield 1;
 }
 
-// experimental:
+// Babel experimental:
 let { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
-console.log('@x',x);
-console.log('@y',y);
-console.log('@z',z);
+console.log('@Babel experimental:')
+console.log('@x:',x);
+console.log('@y:',y);
+console.log('@z:',z);
 
 React.render(React.createElement(App), document.body);
